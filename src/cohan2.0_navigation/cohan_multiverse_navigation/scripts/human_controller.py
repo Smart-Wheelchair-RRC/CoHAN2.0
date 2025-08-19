e43e45e99c13df5881a8bd88b1ec78657f8b0dfc
sphanit <ptsingaman@laas.fr>
2025-08-19 17:03:57 +0200

    updated controller for human

--- src/cohan2.0_navigation/cohan_multiverse_navigation/scripts/human_controller.py
+++ src/cohan2.0_navigation/cohan_multiverse_navigation/scripts/human_controller.py
@@ -3,6 +3,8 @@ import rospy
 import sys
 from geometry_msgs.msg import PoseStamped, Twist
 from nav_msgs.msg import Odometry
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+
 
 class HumanController:
     def __init__(self, x=None, y=None):
@@ -13,18 +15,23 @@ class HumanController:
         _ = rospy.Timer(rospy.Duration(0.1), self.pose_publisher)
 
         ## Set the initial pose of the agent (can be done manually or through the topic)
+        self.human_pose = PoseStamped()
         if x is None or y is None:
-            self.human_pose = PoseStamped()
+            self.human_pose.header.frame_id = "map"
             self.human_pose.pose.position.x = 0
             self.human_pose.pose.position.y = 0
+            self.human_pose.pose.orientation.w = 1.0
         else:
-            self.human_pose = PoseStamped()
+            self.human_pose.header.frame_id = "map"
             self.human_pose.pose.position.x = x
             self.human_pose.pose.position.y = y
-        self.pose_pub.publish(self.human_pose)
+            self.human_pose.pose.orientation.w = 1.0
 
         ## Initial Velocity
         self.human_velocity = Twist()
+
+        rospy.sleep(0.5)
+        self.pose_pub.publish(self.human_pose)
 
     def pose_callback(self, msg):
         rospy.loginfo("Received human pose: %s", msg)
@@ -34,14 +41,10 @@ class HumanController:
     def velocity_callback(self, msg):
         rospy.loginfo("Received human velocity: %s", msg)
         self.human_velocity = msg
-        
         ## Update the human pose based on the velocity
         self.move_human()
-
         ## Publish the updated human pose
         self.pose_pub.publish(self.human_pose)
-
-
 
     def move_human(self):
         """
@@ -50,7 +53,16 @@ class HumanController:
         dt = 0.1  # Time step
         self.human_pose.pose.position.x += self.human_velocity.linear.x * dt
         self.human_pose.pose.position.y += self.human_velocity.linear.y * dt
-        self.human_pose.pose.orientation.z += self.human_velocity.angular.z * dt   
+        _, _, yaw = euler_from_quaternion([self.human_pose.pose.orientation.x,
+                                            self.human_pose.pose.orientation.y,
+                                            self.human_pose.pose.orientation.z,
+                                            self.human_pose.pose.orientation.w])
+        yaw += self.human_velocity.angular.z * dt
+        quat = quaternion_from_euler(0, 0, yaw)
+        self.human_pose.pose.orientation.x = quat[0]
+        self.human_pose.pose.orientation.y = quat[1]
+        self.human_pose.pose.orientation.z = quat[2]
+        self.human_pose.pose.orientation.w = quat[3]
 
     def pose_publisher(self, event):
         """
@@ -71,5 +83,5 @@ if __name__ == '__main__':
         y = float(sys.argv[2])
         controller = HumanController(x, y)
     else:
-        controller = HumanController()
+        controller = HumanController(5, 2)
     rospy.spin()
