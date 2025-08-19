45ac57bacccbd3008cb68f10f6e563fc0cad405b
sphanit <ptsingaman@laas.fr>
2025-08-19 16:19:04 +0200

    added human controller for multiverse_mujoco

---
+++ src/cohan2.0_navigation/cohan_multiverse_navigation/scripts/human_controller.py
@@ -0,0 +1,65 @@
+import rospy
+import sys
+from geometry_msgs.msg import PoseStamped, Twist
+from nav_msgs.msg import Odometry
+
+class HumanController:
+    def __init__(self, x=None, y=None):
+        self.ps = rospy.Subscriber('/initial_human_pose', PoseStamped, self.pose_callback)
+        self.ts = rospy.Subscriber('/human_cmd_vel', Twist, self.velocity_callback)
+        self.pose_pub = rospy.Publisher('/cylinder_with_arrow/pose_stamped', PoseStamped, queue_size=10)
+        self.base_pose_pub = rospy.Publisher('/human_pose', Odometry, queue_size=10)
+
+        ## Set the initial pose of the agent (can be done manually or through the topic)
+        if x is None or y is None:
+            self.human_pose = PoseStamped()
+            self.human_pose.pose.position.x = 0
+            self.human_pose.pose.position.y = 0
+        else:
+            self.human_pose = PoseStamped()
+            self.human_pose.pose.position.x = x
+            self.human_pose.pose.position.y = y
+        self.pose_pub.publish(self.human_pose)
+
+        ## Initial Velocity
+        self.human_velocity = Twist()
+
+    def pose_callback(self, msg):
+        rospy.loginfo("Received human pose: %s", msg)
+        self.human_pose = msg
+        self.pose_pub.publish(self.human_pose)
+
+    def velocity_callback(self, msg):
+        rospy.loginfo("Received human velocity: %s", msg)
+        self.human_velocity = msg
+        
+        ## Update the human pose based on the velocity
+        self.move_human()
+
+        # Publish the base pose + velocity as Odometry
+        odom_msg = Odometry()
+        odom_msg.header.stamp = rospy.Time.now()
+        odom_msg.header.frame_id = "map"
+        odom_msg.pose.pose = self.human_pose.pose
+        odom_msg.twist.twist = self.human_velocity
+        self.base_pose_pub.publish(odom_msg)
+
+    def move_human(self):
+        """
+        Update the human pose based on the current velocity.
+        """
+        dt = 0.1  # Time step
+        self.human_pose.pose.position.x += self.human_velocity.linear.x * dt
+        self.human_pose.pose.position.y += self.human_velocity.linear.y * dt
+        self.human_pose.pose.orientation.z += self.human_velocity.angular.z * dt   
+
+
+if __name__ == '__main__':
+    rospy.init_node('human_controller')
+    if len(sys.argv) > 2:
+        x = float(sys.argv[1])
+        y = float(sys.argv[2])
+        controller = HumanController(x, y)
+    else:
+        controller = HumanController()
+    rospy.spin()
